b1ca5b9f94c96df16de00c64b5779a8a95aa0444
hermitec <bgarden@tutanota.com>
2020-05-01 07:48:49 +0100

    integrate testing into main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from tensorflow.keras.preprocessing import image
 
 #test
 # h,w must be divisible by 4
-h = 24
-w = 24
+h = 32
+w = 32
 channels = 3
 
 def load_preprocess(path):
@@ -47,7 +47,7 @@ def model_vertex_discriminator():
     b_input = layers.Input(shape=(8,3))
     x = layers.Conv1D(64,(1),activation="relu")(b_input)
     x = layers.Flatten()(x)
-    x = layers.Dropout(0.3)(x)                  # Without dropout on D, D will always outtrain G early
+    x = layers.Dropout(0.35)(x)                  # Without dropout on D, D will always outtrain G early
     x = layers.Dense(1, activation="sigmoid")(x)# and G will never learn how to trick D.
 
     model = tf.keras.models.Model(b_input,x)
@@ -65,16 +65,16 @@ def parse(obj_file):
 # Training parameters here (learning rate etc.) are an absolute nightmare
 # and the slightest tweak can make or break the learning process
 vertex_model = model_vertex_creator()
-vertex_optimizer = tf.keras.optimizers.Adam(lr=0.0006, clipvalue=1.0, decay=1e-8,beta_1=0.5)
+vertex_optimizer = tf.keras.optimizers.Adam(lr=0.00075, clipvalue=1.0, decay=1e-8,beta_1=0.5)
 vertex_model.compile(optimizer=vertex_optimizer,loss="binary_crossentropy")
 
 vertex_discriminator = model_vertex_discriminator()
-d_optimizer = tf.keras.optimizers.Adam(lr=0.0004, clipvalue=1.0, decay=1e-8,beta_1=0.5)
+d_optimizer = tf.keras.optimizers.Adam(lr=0.0005, clipvalue=1.0, decay=1e-8,beta_1=0.5)
 vertex_discriminator.compile(optimizer=d_optimizer,loss="binary_crossentropy")
 
 # -- #
 
-data_folder = "./Dataset/"
+data_folder = raw_input("Input folder: ")
 
 raw_data = []
 raw_labels = []
@@ -82,6 +82,8 @@ for i in os.listdir(data_folder):
 
     if len(os.listdir(data_folder+i)) > 0:
         imgdirs = [data_folder+i+"/"+p for p in os.listdir(data_folder+i) if p.split(".")[1] == "png"]
+        for x in imgdirs:
+            print(x)
         imgs = [load_preprocess(x) for x in imgdirs]
         raw_data.append(imgs)
 
@@ -101,7 +103,7 @@ gan.compile(gan_optimizer,loss="binary_crossentropy")
 
 # TRAINING PARAMS #
 
-EPOCHS = 200
+EPOCHS = 10000
 BATCH_SIZE = 1 # Currently useless, need to implement because GANs become
                # far easier to train the larger the batch size.
 
@@ -148,79 +150,72 @@ checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
 raw_data = np.reshape(raw_data, (len(raw_data),6,h,w,channels))
 
-
+history = []
 # Actual training process:
 
-for i in range(EPOCHS):
+training = raw_input("Perform training? y/n :")
 
-    if i % 100 == 0:
+if training == "y":
 
-        # Save progress
-        checkpoint.save(file_prefix = checkpoint_prefix)
+    for i in range(EPOCHS):
 
-        out = []
-        for x in vertex_model.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))).tolist():
-            out.append(x)
-        out_str = "out_at_epoch_{}.obj".format(i)
-        print(out)
+        if i % 100 == 0:
 
-        # Save current predictions of G
-        f = open(out_str,"w+")
-        f.write("o Cube\n")
-        for x in out[0][0:]:
-            f = open(out_str,"a+")
-            print("v {0} {1} {2}\n".format(x[0],x[1],x[2]))
-            f.write("v {0} {1} {2}\n".format(x[0],x[1],x[2]))
+            # Save progress
+            checkpoint.save(file_prefix = checkpoint_prefix)
 
+            out = []
+            for x in vertex_model.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))).tolist():
+                out.append(x)
+            out_str = "./Output/out_at_epoch_{}.obj".format(i)
+            print(out)
 
-    #... and train for another epoch
-
-    # right now all data is trained every epoch which is an absolutely
-    # awful idea
-    generated_objs = vertex_model.predict(raw_data, steps=1)
-    combined_obj = np.concatenate([generated_objs,raw_labels])
-
-    misleading_targets = np.ones((len(generated_objs),1))
-    misleading_targets += -1 * np.random.random(misleading_targets.shape)
-
-    d_loss = vertex_discriminator.train_on_batch(combined_obj, np.concatenate([np.zeros((len(raw_labels))),np.ones((len(raw_labels)))]))
-    a_loss = gan.train_on_batch(raw_data,misleading_targets)
-
-    history.append([d_loss,a_loss])
-    os.system("clear")
-    print(vertex_model.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))))
-    print(raw_labels[0])
-    print("D LOSS: {0}".format(d_loss))
-    print("GAN LOSS: {0}".format(a_loss))
+            # Save current predictions of G
+            f = open(out_str,"w+")
+            f.write("o Cube\n")
+            for x in out[0][0:]:
+                f = open(out_str,"a+")
+                print("v {0} {1} {2}\n".format(x[0],x[1],x[2]))
+                f.write("v {0} {1} {2}\n".format(x[0],x[1],x[2]))
 
 
+        #... and train for another epoch
 
-# -- Saving data after the fact -- #
+        # right now all data is trained every batch which is an absolutely
+        # awful idea
+        generated_objs = vertex_model.predict(raw_data, steps=1)
+        combined_obj = np.concatenate([generated_objs,raw_labels])
 
-f = open("graph_raw.txt","w+")
-for l in [(str(history[x][0])+","+str(history[x][1])+";\n") for x,i in enumerate(history)]:
-    f = open("graph_raw.txt","a+")
-    f.write(l)
+        misleading_targets = np.ones((len(generated_objs),1))
+        misleading_targets += -1 * np.random.random(misleading_targets.shape)
+
+        d_loss = vertex_discriminator.train_on_batch(combined_obj, np.concatenate([np.zeros((len(raw_labels))),np.ones((len(raw_labels)))]))
+        a_loss = gan.train_on_batch(raw_data,misleading_targets)
+
+        history.append([d_loss,a_loss])
+        os.system("clear")
+        print(vertex_model.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))))
+        print(raw_labels[0])
+        print("D LOSS: {0}".format(d_loss))
+        print("GAN LOSS: {0}".format(a_loss))
+
+
+
+    # -- Saving data after the fact -- #
+
+    f = open("graph_raw.txt","w+")
+    for l in [(str(history[x][0])+","+str(history[x][1])+";\n") for x,i in enumerate(history)]:
+        f = open("graph_raw.txt","a+")
+        f.write(l)
+
 out = []
 for i in vertex_model.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))).tolist():
     out.append(i)
 
 print(out)
-f = open("out.obj","w+")
+f = open("final_out.obj","w+")
 f.write("o Cube\n")
 for i in out[0][0:]:
-    f = open("out.obj","a+")
-    print("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
-    f.write("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
-
-out = []
-for i in vertex_model.predict(np.array(raw_data[1]).reshape((1,6,w,h,channels))).tolist():
-    out.append(i)
-
-print(out)
-f = open("out1.obj","w+")
-f.write("o Cube\n")
-for i in out[0][0:]:
-    f = open("out1.obj","a+")
+    f = open("final_out.obj","a+")
     print("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
     f.write("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
