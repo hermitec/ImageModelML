fca1e5726ce5971590a9ae1ef17fe0ba34455dde
hermitec <bgarden@tutanota.com>
2020-10-13 12:10:18 +0100

    weee

---
+++ html/config.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        "database" => [
+            "host" => "localhost:3306",
+            "name" => "login_info",
+            "user" => "root",
+            "password" => ""
+        ]
+    ];
+?>
--- html/main.php
+++ html/main.php
@@ -1,1 +1,26 @@
-hi
+<?php
+    
+    session_start();
+
+    $badChars = array( '"', "'", "`", ";", "-" );
+    $username = str_replace( $badChars, "", $_POST["user"] );
+    $password = str_replace( $badChars, "", $_POST["pw"] );
+
+    $_SESSION["user"] = $username;
+    $_SESSION["loggedin"] = 0;
+
+    include 'sql.php';
+    $results = $db -> query( "SELECT * FROM Users" );
+
+    while ( $row = mysqli_fetch_array( $results )  ) {
+        if ( $row[0] == $username && $row[1] == $password ){
+            $_SESSION["loggedin"] = 1;
+            echo $_SESSION["loggedin"];
+            exit();
+        } else {
+            $_SESSION["loggedin"] = 0;
+        }
+    }
+
+    mysqli_close( $db );
+?>
