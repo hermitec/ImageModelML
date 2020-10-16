2e37c774183915d0281acb0a98226de24fe12ded
hermitec <bgarden@tutanota.com>
2020-10-16 10:21:33 +0100

    T WORKS I THINK

--- html/temptest.php
+++ html/temptest.php
@@ -1,6 +1,6 @@
 <?php
     $config = include( "config.php" );
-    $db = mysqli_connect( "localhost", "newuser", "password:", "login" );
+    $db = mysqli_connect( "localhost", "newuser", "password", "login" );
     if($db == FALSE){
         echo "DATABASE DEAD";
         exit();
