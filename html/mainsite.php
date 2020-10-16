180f112adafecbe003a9f1416941cbf97f6a1371
hermitec <bgarden@tutanota.com>
2020-10-16 10:45:30 +0100

    start work on main site

--- html/mainsite.php
+++ html/mainsite.php
@@ -1,7 +1,52 @@
 <?php
 session_start();
 if($_SESSION["loggedin"] != 1){header("http://18.222.183.14/");exit();}
-?><html>
-    <h1>YOU DID IT</h1>
-    you cracked the code and you're in!!! 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+      
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/stylesheet.css">
+    
+    <title>Image to Model</title>
+  </head>
+  <body>
+    <form method="post">
+        <div class="vertical-center">
+        <div class="card mx-auto" style="width: 18rem;">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="username">X-1</label>
+                        <input type="image" class="form-control" id="x1" placeholder="Enter username" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">X-2</label>
+                        <input type="image" class="form-control" id="password" placeholder="Enter password" name="password">
+                    </div>
+                    <p id="errorText" style="color:red; text-align: center;"></p>
+                    <div class="form-group">
+                        <label for="y1">Y-1</label>
+                        <input type="image" class="form-control" id="y1" placeholder="Enter username" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="y2">Y-2</label>
+                        <input type="image" class="form-control" id="y2" placeholder="Enter password" name="password">
+                    </div>
+                    <a href="#" class="btn btn-primary" style="width:100%;" onclick="log_in()">Submit</a>
+                    <p id="errorText" style="color:red; text-align: center;"></p>
+                </div>
+            </div>
+        </div>
+    </form>
+    <script src="./script/libraries.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body >
+
 </html>
