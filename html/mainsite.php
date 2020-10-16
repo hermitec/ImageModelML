32bd9a7096623e3aea165fca57690b2c6130baf9
hermitec <bgarden@tutanota.com>
2020-10-16 10:41:01 +0100

    redirect if not logged in

--- html/mainsite.php
+++ html/mainsite.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if($_SESSION["loggedin"] != 1){header("http://18.222.183.14/");}
+if($_SESSION["loggedin"] != 1){header("http://18.222.183.14/");exit();}
 ?><html>
     <h1>YOU DID IT</h1>
     you cracked the code and you're in!!! 
