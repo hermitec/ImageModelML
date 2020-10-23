3f4290ffbda8b50ec07d2cb0ba00da4901f6b6a0
hermitec <bgarden@tutanota.com>
2020-10-23 09:47:14 +0100

    asa

--- html/script/libraries.js
+++ html/script/libraries.js
@@ -61,7 +61,7 @@ async function model_compute(){
 
         loginHTTPReq.open( method, url, true );
     loginHTTPReq.setRequestHeader('Content-Description', 'File Transfer');
-    loginHTTPReq.setRequestHeader( "Content-Type", "text/html; charset=UTF-8" );
+    loginHTTPReq.setRequestHeader( "Content-Type", "text/plain; charset=UTF-8" );
     loginHTTPReq.onreadystatechange = function() {
         console.log( this.responseText );
     }
