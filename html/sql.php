cc1c1d5307dbd57708f2213a2b674ddabc9d98a5
hermitec <bgarden@tutanota.com>
2020-12-24 08:26:28 +0000

    you know the rules and so do I

--- html/sql.php
+++ html/sql.php
@@ -1,4 +1,8 @@
 <?php
     $config = include( "config.php" );
     $db = mysqli_connect("localhost", "newuser", "password", "login" );
+    if (mysqli_connect_errno())
+    {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
 ?>
