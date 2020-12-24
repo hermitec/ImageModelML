f34571466979c0b9c996e55327c47b0d078b1a4d
hermitec <bgarden@tutanota.com>
2020-12-24 09:18:04 +0000

    hahahahahahaha

--- html/sql.php
+++ html/sql.php
@@ -1,4 +1,10 @@
 <?php
     $config = include( "config.php" );
     $db = mysqli_connect( $config["database"]["host"], $config["database"]["user"], $config["database"]["password"], $config["database"]["name"] );
+    if (!$db) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 ?>
