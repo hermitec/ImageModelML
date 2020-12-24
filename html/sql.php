37d112ca40cfc4a9356d55bd389c6eed96ea5775
hermitec <bgarden@tutanota.com>
2020-12-24 08:21:16 +0000

    god

--- html/sql.php
+++ html/sql.php
@@ -1,4 +1,4 @@
 <?php
     $config = include( "config.php" );
-    $db = mysqli_connect( $config["database"]["host"], $config["database"]["user"], $config["database"]["password"], $config["database"]["name"] );
+    $db = mysqli_connect("localhost", "newuser", "password", "login" );
 ?>
