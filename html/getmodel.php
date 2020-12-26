1497718f7b97c8ad09e568b9d17592a0ef3970f8
hermitec <bgarden@tutanota.com>
2020-12-26 08:34:45 +0000

    w

--- html/getmodel.php
+++ html/getmodel.php
@@ -1,7 +1,18 @@
 <?php
-$file = "./testfile.txt";
-header('Content-Disposition: attachment; filename="file.txt"');
-$out = `python3 /main.py -s`;
-readfile($file);
-exit();
+$file = 'testfile.txt';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
 ?>
