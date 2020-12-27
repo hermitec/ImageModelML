f323cbdd2c87e7e708be6ab06858f3cbaf9fb2b6
hermitec <bgarden@tutanota.com>
2020-12-27 18:46:49 +0000

    oh haha

--- html/getmodel.php
+++ html/getmodel.php
@@ -1,7 +1,7 @@
 <?php
 $file = "./testfile.obj";
 putenv('PATH=/usr/local/bin');
-$debg = `sudo python3 serversideout.py`
+$debg = `sudo python3 ./serversideout.py`;
 $out = `sudo docker run -it --rm -v /tmp -w /tmp tensorflow/tensorflow python ./main.py -s`;
 echo "hi";
 exit;
