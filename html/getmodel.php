bbba98370da7054b8ab1501de29f50be12bb06c7
hermitec <bgarden@tutanota.com>
2020-12-28 11:44:25 +0000

    no

--- html/getmodel.php
+++ html/getmodel.php
@@ -1,6 +1,6 @@
 <?php
 
-$out = `docker run -it --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow python ./main.py -s 2>&1`;
+$out = `docker run --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow python ./main.py -s 2>&1`;
 echo $out;
 exit;
 ?>
