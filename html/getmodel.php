94fbc19ba452cf1147b777942f16eec662af154e
hermitec <bgarden@tutanota.com>
2020-12-28 09:57:07 +0000

    aaaa

--- html/getmodel.php
+++ html/getmodel.php
@@ -1,8 +1,4 @@
 <?php
-if function_exists('shell_exec'){
-  echo "yes";
-}
-else{echo "no";}
-$out = `sudo docker run -it --rm -v /tmp -w /tmp tensorflow/tensorflow python ./main.py -s`;
+$out = `sudo docker run -it --rm -v $PWD:/tmp -w /tmp tensorflow/tensorflow python ./main.py -s`;
 exit;
 ?>
