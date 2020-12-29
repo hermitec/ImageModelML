be50ff3b66a28a4954f1b62a50cc55cc1c92a9ef
hermitec <bgarden@tutanota.com>
2020-12-29 10:41:51 +0000

    aa

--- html/mainsite.php
+++ html/mainsite.php
@@ -56,14 +56,15 @@ if($_SESSION["loggedin"] != 1){header("http://3.139.70.139");exit();}
          document.getElementById(docID).onchange = function(event) {
          var fileList = document.getElementById(docID).files;
          console.log(fileList);
-    }
+       }
+     }
     setAutoUpload("x1");
     setAutoUpload("x2");
     setAutoUpload("y1");
     setAutoUpload("y2");
     setAutoUpload("z1");
     setAutoUpload("z2");
-}</script>
+</script>
     <script src="./script/libraries.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
