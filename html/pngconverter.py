5557cffb9e924559cde3957ff5c219b3d0f70201
hermitec <bgarden@tutanota.com>
2021-01-01 08:48:08 +0000

    a

--- html/pngconverter.py
+++ html/pngconverter.py
@@ -1,3 +1,15 @@
 import png, os, sys
 f = open("test","r").readlines()
 print(f)
+rgb = []
+currentPixel = []
+ticker = 0 # my personal favourite bad practice
+for i,x in enumerate(f):
+    if ticker == 0:
+        currentPixel = []
+    currentPixel.append(x)
+    if ticker == 2:
+        rgb.append(currentPixel)
+
+image = png.from_array(rgb)
+image.save("testout.png")
