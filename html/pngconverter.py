85b8b6efe8f3dc680d790d1ac9332262e40f274a
hermitec <bgarden@tutanota.com>
2021-01-01 10:12:25 +0000

    PLEASE

--- html/pngconverter.py
+++ html/pngconverter.py
@@ -5,18 +5,6 @@ from io import BytesIO
 f = open("test","r")
 data = f.read()
 f.close()
-
-def decode_base64(data, altchars=b'+/'):
-    """Decode base64, padding being optional.
-
-    :param data: Base64 data as an ASCII byte string
-    :returns: The decoded byte string.
-
-    """
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
-    missing_padding = len(data) % 4
-    if missing_padding:
-        data += b'='* (4 - missing_padding)
-    return base64.b64decode(data, altchars)
-im = Image.open(BytesIO(decode_base64(data[22:])))
+data = bytes(data, "utf-8")
+im = Image.open(BytesIO(base64.b64decode(data)))
 im.save('final.png', 'PNG')
