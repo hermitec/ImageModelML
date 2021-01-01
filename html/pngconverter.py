ce7e62a5a5cc0d1dc92fec31e3fb3498cd6a96f0
hermitec <bgarden@tutanota.com>
2021-01-01 10:18:13 +0000

    a

--- html/pngconverter.py
+++ html/pngconverter.py
@@ -5,6 +5,9 @@ from io import BytesIO
 f = open("test","r")
 data = f.read()
 f.close()
+data = data[22:]
+pad = len(data)%4
+data += b"="*pad
 data = bytes(data, "utf-8")
 im = Image.open(BytesIO(base64.b64decode(data)))
 im.save('final.png', 'PNG')
