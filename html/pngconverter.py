9ebeff530cb00a70ae63c88545120d0d83629c64
hermitec <bgarden@tutanota.com>
2021-01-01 10:19:51 +0000

    oopsie woopsie

--- html/pngconverter.py
+++ html/pngconverter.py
@@ -6,8 +6,8 @@ f = open("test","r")
 data = f.read()
 f.close()
 data = data[22:]
+data = bytes(data, "utf-8")
 pad = len(data)%4
 data += b"="*pad
-data = bytes(data, "utf-8")
 im = Image.open(BytesIO(base64.b64decode(data)))
 im.save('final.png', 'PNG')
