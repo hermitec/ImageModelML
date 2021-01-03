d4684a0596bb347196238ed0560879d9b61a186e
hermitec <bgarden@tutanota.com>
2021-01-03 09:10:58 +0000

    cmon

--- html/uploadfile.php
+++ html/uploadfile.php
@@ -3,5 +3,5 @@ session_start();  //$_POST["file"] is file to upload
 $up = fopen($_POST["filename"], "w");
 fwrite($up, $_POST["file"]);
 fclose($up);
-$out = `python3 pngconverter.py`;
+$out = shell_exec("python3 pngconverter.py " + $_POST["filename"]);
 ?>
