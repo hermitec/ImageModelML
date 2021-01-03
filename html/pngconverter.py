58289052ee280ecd013ffc3ab1afc56a8442a4fc
hermitec <bgarden@tutanota.com>
2021-01-03 09:09:06 +0000

    wow it worked first try okay i guess

--- html/pngconverter.py
+++ html/pngconverter.py
@@ -2,7 +2,7 @@ import base64
 from PIL import Image
 import io
 
-f = open("test","r")
+f = open(sys.argv[0],"r")
 data = f.read()
 f.close()
 data = data[22:].replace(" ","+")
@@ -10,4 +10,4 @@ data = bytes(data, "utf-8")
 pad = len(data)%4
 data += b"="*pad
 im = Image.open(io.BytesIO(base64.b64decode(data)))
-im.save('final.png', 'PNG')
+im.save('{}.png'.format(sys.argv[0]), 'PNG')
