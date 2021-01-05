f4d60f7ee0463c4cb74b120f095cad97bc0c8ea9
hermitec <bgarden@tutanota.com>
2021-01-05 11:08:05 +0000

    BRUH

--- html/main.py
+++ html/main.py
@@ -198,7 +198,6 @@ with tf.device('/gpu:0'):
         out = []
         for i in G.x.predict(np.array(raw_data[0]).reshape((1,6,w,h,channels))).tolist():
             out.append(i)
-
         f = open("testfile.obj","w+")
         f.write("o Cube\n")
         f.close()
@@ -206,14 +205,13 @@ with tf.device('/gpu:0'):
             f = open("testfile.obj","a+")
             print("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
             f.write("v {0} {1} {2}\n".format(i[0],i[1],i[2]))
-            f.close()
-        f = open("testfile.obj","a+")
         f.write("""f 1/1/1 5/2/1 7/3/1 3/4/1
         f 4/5/2 3/4/2 7/6/2 8/7/2
         f 8/8/3 7/9/3 5/10/3 6/11/3
         f 6/12/4 2/13/4 4/5/4 8/14/4
         f 2/13/5 1/1/5 3/4/5 4/5/5
         f 6/11/6 5/10/6 1/1/6 2/13/6""")
+        f.close()
         sys.exit()
 
     update_batch()
